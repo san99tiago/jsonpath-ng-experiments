31cc892b4dcb9af012f1e84a95518aaa9aec9b82
Santiago Garcia Arango <santiago.garcia1999@hotmail.com>
2023-08-30 22:16:16 -0500

    Update unit tests with validator and sample payload and response for good/bad cases

--- tests/unit/mapper/test_validator.py
+++ tests/unit/mapper/test_validator.py
@@ -7,24 +7,25 @@ def test_validator_success():
     from src.mapper.validator import Validator
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    payload_path = os.path.join(current_dir, "payload_good_01.json")
+    payload_path = os.path.join(current_dir, "source_payload_good_01.json")
 
     with open(payload_path, "r") as payload_file:
         payload = json.load(payload_file)
 
-    Validator(payload)
+    validator = Validator()
+    assert validator.validate_payload(payload) is True
 
 
 def test_validator_failure():
     from src.mapper.validator import Validator
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    payload_path = os.path.join(current_dir, "payload_bad_01.json")
+    payload_path = os.path.join(current_dir, "source_payload_bad_01.json")
 
     with open(payload_path, "r") as payload_file:
         payload = json.load(payload_file)
 
-    with pytest.raises(Exception) as e:
-        Validator(payload)
+    validator = Validator()
+    result = validator.validate_payload(payload)
 
-    assert "Payload validation error" in str(e.value)
+    assert result["Error"] == "Payload schema validation error"
